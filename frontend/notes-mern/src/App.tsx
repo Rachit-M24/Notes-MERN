1ceec6c797654462f7bc20b18dffdf33bbea12fe
Rachit-M24 <rachitm2022@gmail.com>
2026-05-15 19:07:41 +0530

    #5 - Created the frontend application 2.Integrated the tailwind CSS 3.cleaned up the not needed files from the code folder 4. replaced the default code from the startup files

---
+++ frontend/notes-mern/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="h-full w-full text-white p-2  bg-amber-700 text-3xl font-bold underline text-center mt-10">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
